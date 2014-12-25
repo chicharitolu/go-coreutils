38940009373779d6f6e035d60b3d9d9a0de3f65b
chichariotlu <luhancheng@baidu.com>
2014-12-25 14:40:59 +0800

    true: help and version

--- true/true.go
+++ true/true.go
@@ -1,7 +1,41 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"flag"
+	"os"
+)
 
 func main() {
+	help_flag := flag.Bool("help", false, "help flag")
+	version_flag := flag.Bool("version", false, "version flag")
+	
+	flag.Parse()
+	if flag.NFlag() > 1 {
+		os.Exit(0)
+	}
+
+	if *help_flag {
+		usage()
+	}
+
+	if *version_flag {
+		version()
+	}
 	os.Exit(0)
+}
+
+func usage () {
+	description :=`Usage: true [ignored command line arguments]
+  or:  true OPTION
+Exit with a status code indicating success.
+
+      --help     display this help and exit
+      --version  output version information and exit
+`
+	fmt.Print(description)
+}
+
+func version (){
+	fmt.Print("true (go-coreutils) 0.01\n")
 }
